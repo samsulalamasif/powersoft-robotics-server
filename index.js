6c23ac5bc6e47f453209b9fea603da8bd7431832
samsulalamasif <samsulalamasif@gmail.com>
2023-05-18 16:52:30 +0600

    toy post and get method added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -5,6 +6,67 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vqy99hm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const toyCollection = client.db("PowersoftRobotics").collection("addToy")
+
+
+        // ----add toy----
+        app.post("/addToy", async (req, res) => {
+            const body = req.body
+            // console.log(body);
+            const result = await toyCollection.insertOne(body)
+            res.send(result)
+        })
+
+
+        // all toy show
+
+        app.get("/allToys", async (req, res) => {
+            const cursor = toyCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
